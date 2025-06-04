90bcc836244a1475cb29def864b2158310d54386
Trupti Bhatlekar <bhatlekar.trupti@gmail.com>
2025-06-04 21:33:55 +0530

    Pattern_1.c

--- Assignment_17/Pattern_1.c
+++ Assignment_17/Pattern_1.c
@@ -24,25 +24,23 @@ void Pattern(int iRow, int iCol)
     for (j = 1; j <= iCol; j++)
     {
       if (j == (iCol - i + 1))
-
+      {
         printf("# ");
-
+      }
       else
-
+      {
         printf("* ");
+      }
     }
     printf("\n");
   }
 }
 
 int main()
-
 {
-
   int iValue1 = 0, iValue2 = 0;
 
   printf("Enter number of rows and columns:");
-
   scanf("%d %d", &iValue1, &iValue2);
 
   Pattern(iValue1, iValue2);
