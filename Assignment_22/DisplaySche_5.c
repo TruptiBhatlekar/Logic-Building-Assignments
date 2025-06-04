8ee6a4deee6666108f864ecb195f7433010d3759
Trupti Bhatlekar <bhatlekar.trupti@gmail.com>
2025-06-04 22:03:29 +0530

    DisplaySche_5.c

--- Assignment_22/DisplaySche_5.c
+++ Assignment_22/DisplaySche_5.c
@@ -20,16 +20,25 @@ typedef int BOOL;
 BOOL DisplaySchedule(char chDiv)
 {
   if (chDiv == 'A' || chDiv == 'a')
-    {printf("Your exam at 7.00 AM\n");}
+  {
+    printf("Your exam at 7.00 AM\n");
+  }
   else if (chDiv == 'B' || chDiv == 'b')
+  {
     printf("Your exam at 8.30 AM\n");
+  }
   else if (chDiv == 'C' || chDiv == 'c')
+  {
     printf("Your exam at 9.20 AM\n");
+  }
   else if (chDiv == 'D' || chDiv == 'd')
+  {
     printf("Your exam at 10.30 AM\n");
+  }
   else
+  {
     printf("Invalid Division\n");
-
+  }
   return TRUE;
 }
 
