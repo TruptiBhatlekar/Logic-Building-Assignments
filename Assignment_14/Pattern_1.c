52962ae8b8587504960cf70529f8a47cd558bd4f
Trupti Bhatlekar <bhatlekar.trupti@gmail.com>
2025-06-04 21:29:25 +0530

    Pattern_1.c

--- Assignment_14/Pattern_1.c
+++ Assignment_14/Pattern_1.c
@@ -20,17 +20,19 @@ void Pattern(int iRow, int iCol)
   int i = 0, j = 0, num = 1;
 
   for (i = 1; i <= iRow; i++)
-  {
+{
     for (j = 1; j <= iCol; j++)
-    {
+  {
       printf("%d\t", num);
       num++;
 
       if (num == 10)
+      {
         num = 1;
-    }
-    printf("\n");
+      }
   }
+    printf("\n");
+ }
 }
 
 int main()
